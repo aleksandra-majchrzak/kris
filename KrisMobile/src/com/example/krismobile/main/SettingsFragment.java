7e5def962b64a0e8219c2943e828dbc20d7a477c
Aleksandra Majchrzak <briza.6@gmail.com>
2016-02-08 18:47:15 +0100

    Added listener on server number change in settings.

--- KrisMobile/src/com/example/krismobile/main/SettingsFragment.java
+++ KrisMobile/src/com/example/krismobile/main/SettingsFragment.java
@@ -1,12 +1,22 @@
 package com.example.krismobile.main;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.krismobile.R;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
 
+	private static String SERVER_ADDRESS_PREF = "server_address_preference";
+	
+	private EditTextPreference serverAddressPref;
+	
 	public SettingsFragment(){
 		
 	}
@@ -17,6 +27,44 @@ public class SettingsFragment extends PreferenceFragment {
 
 		// Load the preferences from an XML resource
 		addPreferencesFromResource(R.xml.settings);
+    }
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View rootView = super.onCreateView(inflater, container, savedInstanceState);
+		
+		serverAddressPref = (EditTextPreference) findPreference(SERVER_ADDRESS_PREF);
+		serverAddressPref.setSummary(serverAddressPref.getText().toString());
+		
+		
+		return rootView;
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		
+		if(key.equals(SERVER_ADDRESS_PREF)){
+		//	EditTextPreference serverAddressPref = (EditTextPreference) findPreference(SERVER_ADDRESS_PREF);
+			serverAddressPref.setSummary(serverAddressPref.getText().toString());
+		}
+		
+	}
+	
+	@Override
+    public void onResume() {
+        super.onResume();
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 
 }
